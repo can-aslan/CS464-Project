90d3d8018dd35d20fd1848d5f782bd0eefa27327
nisayilmaz <nisayilmaz221@gmail.com>
2024-04-20 00:37:15 +0300

    Decision tree added

---
+++ decision_tree.py
@@ -0,0 +1,68 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn import tree
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import validation_curve, cross_val_score
+import matplotlib.pyplot as plt
+
+
+data = pd.read_csv('dataset_link_phishing.csv', low_memory = False)
+data = pd.DataFrame(data)
+unique_value_counts = {col: data[col].nunique() for col in data.columns}
+columns_to_drop = [col for col, count in unique_value_counts.items() if count == 1]
+data.drop(columns=columns_to_drop, inplace=True)
+data = data.drop(columns=['Unnamed: 0'])
+data.drop(columns=['url'], inplace=True)
+data = pd.DataFrame(data)
+data['status'] = data['status'].map({'legitimate': 1, 'phishing': 0})
+data['domain_with_copyright'] = data['domain_with_copyright'].map({'one': 1, 'zero': 0, 'One': 1, 'Zero': 0, '1': 1, '0': 0})
+
+x = data.copy()
+X = x.drop('status', axis=1)
+y = data['status']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
+
+depths = np.arange(1, 31, 1)  
+depths[0] = 1
+
+clf = DecisionTreeClassifier(random_state=0)
+
+train_scores, valid_scores = validation_curve(
+    clf, X_train, y_train,
+    param_name='max_depth', param_range=depths, cv=5
+)
+
+train_scores_mean = np.mean(train_scores, axis=1)
+valid_scores_mean = np.mean(valid_scores, axis=1)
+
+plt.figure(figsize=(10, 6))
+plt.title("Validation Curve with Decision Tree (max_depth)")
+plt.xlabel("max_depth")
+plt.ylabel("Score")
+plt.ylim(0.0, 1.1)
+plt.plot(depths, train_scores_mean, label="Training score", color="darkorange", marker='o')
+plt.plot(depths, valid_scores_mean, label="Cross-validation score", color="navy", marker='o')
+plt.legend(loc="best")
+plt.show()
+
+best_depth = depths[np.argmax(valid_scores_mean)]
+print("Best Depth:", best_depth)
+
+best_clf = DecisionTreeClassifier(max_depth=best_depth, random_state=0)
+best_clf.fit(X_train, y_train)
+accuracy = cross_val_score(best_clf, X_test, y_test, cv=5).mean()
+print("Accuracy with Best Depth:", accuracy)
+
+feature_names = X.columns.tolist()
+
+target_names = ['phishing', 'legitimate']
+
+fig = plt.figure(figsize=(15,10))
+_ = tree.plot_tree(best_clf, 
+                   feature_names=feature_names,  
+                   class_names=target_names,
+                   filled=True)
+plt.show()
