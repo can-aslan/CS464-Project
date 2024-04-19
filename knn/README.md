303c494ae076e36a6b85f9e3516cb8c7663d8888
Yagiz Can Aslan <can.aslan@ug.bilkent.edu.tr>
2024-04-19 23:43:32 +0300

    Implemented first version of KNN

---
+++ knn/README.md
@@ -0,0 +1,14 @@
+## CS464 Project - K Nearest Neighbours (KNN)
+Please follow the instructions below to run the source code.
+
+### Ensure you have Docker installed
+Check https://www.docker.com/ for more details.
+
+### Ensure terminal directory
+The terminal must be in "knn" `cd knn`
+
+### Build the Docker image
+`docker build -t knn .`
+
+### Run the Docker container
+`docker run -it --rm --name knn knn`
---
+++ knn/knn.py
@@ -0,0 +1,65 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.neighbors import KNeighborsClassifier as KNN
+from sklearn.preprocessing import StandardScaler as SS
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+
+def main():
+    # Load the data
+    data = pd.read_csv("data.csv", low_memory=False)
+
+    # Modify features
+    data["status"] = data["status"].apply(lambda x: 1 if x == "phishing" else 0)
+    data["domain_with_copyright"] = data["domain_with_copyright"].apply(lambda x: 1 if x == "one" else 0)
+
+    if "url" in data.columns:
+        data = data.drop("url", axis=1)
+
+    X = data.drop("status", axis=1)
+    y = data["status"]
+
+    # Split data into training and testing sets
+    train_x, test_x, train_y, test_y = train_test_split(X, y, test_size=0.2, random_state=42)
+
+    # Standardize the features
+    scaler = SS()
+    train_x = scaler.fit_transform(train_x)
+    test_x = scaler.transform(test_x)
+
+    min_k = 1
+    max_k = len(train_y) // 2
+
+    # Generate k values using numpy logspace
+    num_points = 20
+    range_of_k = np.logspace(np.log10(min_k), np.log10(max_k), num=num_points, base=10).astype(int)
+    range_of_k = np.unique(range_of_k) # ensures all k values are unique and covers up to max_k
+
+    accuracies = []
+    k_values = []
+
+    # Iterate over the generated range of k values
+    for k in range_of_k:
+        knn = KNN(n_neighbors=k)
+        knn.fit(train_x, train_y)
+
+        pred_y = knn.predict(test_x)
+        accuracy = accuracy_score(test_y, pred_y)
+
+        accuracies.append(accuracy)
+        k_values.append(k)
+
+        print(f"K:{k} | Accuracy: {accuracy * 100:.3f}%")
+
+    # Plot the results
+    plt.figure(figsize=(10, 5))
+    plt.plot(k_values, accuracies, marker="o", linestyle="-", color="b")
+    plt.title("K-Value vs. Accuracy")
+    plt.xlabel("Number of Neighbors: K")
+    plt.ylabel("Accuracy")
+    plt.grid(True)
+    plt.close()
+    return
+
+main()
